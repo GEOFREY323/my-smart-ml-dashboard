e4bdea359eed6171d051c1fcb9b884bf25a3320c
unknown <gamgeofreyankinimbom@gmail.com>
2025-08-28 15:02:29 +0100

    fix typo error

--- smart.py
+++ smart.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sckit
-from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.preprocessing import LabelEncoder,StandardScaler,MinMaxScaler
+from sklearn.linear_model import LinearRegression,Ridge,Lasso
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
